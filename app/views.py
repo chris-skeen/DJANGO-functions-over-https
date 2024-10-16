3e62bc30caa2ac8a1844e626d7d927d68bd68b90
Chris Skeen <chrisskeen2006@gmail.com>
2024-10-16 13:47:18 -0500

    all changes

---
+++ app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+
+# Create your views here.
+
+def hey_view(request: HttpRequest, name: str) -> HttpResponse:
+  return HttpResponse(f'Hey {name}!')
+
+def age_in_view(request: HttpRequest, end: int, birthyear: int) -> HttpResponse:
+  age = end - birthyear
+  return HttpResponse(f'{age}')
+
+def order_total_view(request: HttpRequest, burgers: int, fries: int, drinks: int) -> HttpResponse:
+  burgers_price = 4.50 * burgers
+  fries_price = 1.50 * fries
+  drinks_price = 1.00 * drinks
+
+  total = burgers_price + fries_price + drinks_price
+  return HttpResponse(f'Order Total: {total:.2f}$')
---
+++ config/urls.py
@@ -0,0 +1,27 @@
+"""
+URL configuration for config project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+from app.views import hey_view, age_in_view, order_total_view
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("hey/<str:name>", hey_view),
+    path("age-in/<int:end>/<int:birthyear>/", age_in_view),
+    path("order-total/<int:burgers>/<int:fries>/<int:drinks>/", order_total_view)
+]
